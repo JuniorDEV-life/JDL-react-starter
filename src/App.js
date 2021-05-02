5c22b32fb28ad9cce951d507a5ab352072912f65
Can Soylu <hsyncn17@gmail.com>
2021-05-03 00:36:10 +0300

    added router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={(routerProps) => <HomePage {...routerProps} />}
+        />
+        <Route
+          path="/profile"
+          component={(routerProps) => <ProfilePage {...routerProps} />}
+        />
+        <Route
+          path="/login"
+          component={(routerProps) => <AuthPage {...routerProps} />}
+        />
+      </Switch>
+    </Router>
   );
 }
 
